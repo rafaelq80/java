7bfdd11307a9b413c9c966054b438c4601ddaa10
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-03-14 12:51:36 -0300

    Projeto Hello World

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		int teste = 10;
+		
+		System.out.println("Hello World!");
+		System.out.println("\nOlá Turma 62!");
+		System.out.println("Variável teste: " + teste);
+	}
+
+}
